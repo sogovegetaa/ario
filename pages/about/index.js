587ff0aa5d94dbb7bf5cb8a025b4acf4c40b7871
kuaysh <mustakhkuanysh@gmail.com>
2023-01-06 14:06:47 +0600

    add rekvizity for about2

--- pages/about/index.js
+++ pages/about/index.js
@@ -19,17 +19,17 @@ const About = () => {
       <Navbar />
       <div className="bg-[url('../public/title-bg.png')] bg-no-repeat bg-center bg-cover">
         <div className="container mx-auto">
-          <h1 className="text-4xl font-bold py-8">{t.nav2}</h1>
+          <h1 className="py-8 text-4xl font-bold">{t.nav2}</h1>
         </div>
       </div>
       <div className="py-10">
-        <div className="container mx-auto py-5">
-          <div className="grid grid-cols-3 gap-5 items-center">
+        <div className="container py-5 mx-auto">
+          <div className="grid items-center grid-cols-3 gap-5">
             <div className="col-span-1">
               <div className="text-3xl text-center">{t.abouttitle}</div>
             </div>
             <div className="col-span-2">
-              <div className="text-xl flex pb-5">
+              <div className="flex pb-5 text-xl">
                 <div>
                   <FlagIcon
                     color="info"
@@ -39,7 +39,7 @@ const About = () => {
                 </div>
                 <div>{t.aboutsubtitle1}</div>
               </div>
-              <div className="text-xl flex">
+              <div className="flex text-xl">
                 <div>
                   <ReceiptLongIcon
                     color="info"
@@ -55,47 +55,47 @@ const About = () => {
       </div>
       <hr />
       <div className="container mx-auto ">
-        <div className="bg-gray-200 p-5 mt-5 rounded-lg mb-5">
+        <div className="p-5 mt-5 mb-5 bg-gray-200 rounded-lg">
           <p className="text-[#275274] text-3xl mt-11 text-left font-bold">
-            <span className="text-red-900 dont-bold text-5xl font-mono pr-5">
+            <span className="pr-5 font-mono text-5xl text-red-900 dont-bold">
               01
             </span>
             {t.start}
           </p>
-          <div className="grid grid-cols-2 gap-10 container mx-auto pt-8 text-md pb-8 items-center">
+          <div className="container grid items-center grid-cols-2 gap-10 pt-8 pb-8 mx-auto text-md">
             <p className="leading-6">{t.start1}</p>
             <p className="leading-6">{t.start2}</p>
           </div>
         </div>
         <hr />
-        <div className="bg-gray-200 p-5 mt-5 rounded-lg mb-5">
+        <div className="p-5 mt-5 mb-5 bg-gray-200 rounded-lg">
           <p className="text-[#275274] text-3xl mt-11 text-left font-bold">
-            <span className="text-red-900 dont-bold text-5xl font-mono pr-5">
+            <span className="pr-5 font-mono text-5xl text-red-900 dont-bold">
               02
             </span>
             {t.activity}
           </p>
-          <div className="grid grid-cols-2 gap-10 container mx-auto pt-8 text-md pb-8 items-center">
+          <div className="container grid items-center grid-cols-2 gap-10 pt-8 pb-8 mx-auto text-md">
             <p className="leading-6">{t.activity1}</p>
             <p className="leading-6">{t.activity2}</p>
           </div>
         </div>
         <hr />
-        <div className="bg-gray-200 p-5 mt-5 rounded-lg mb-5">
+        <div className="p-5 mt-5 mb-5 bg-gray-200 rounded-lg">
           <p className="text-[#275274] text-3xl mt-11 text-left font-bold">
-            <span className="text-red-900 dont-bold text-5xl font-mono pr-5">
+            <span className="pr-5 font-mono text-5xl text-red-900 dont-bold">
               03
             </span>
             {t.relevance}
           </p>
-          <div className="grid grid-cols-2 gap-10 container mx-auto pt-8 text-md pb-8 items-center">
+          <div className="container grid items-center grid-cols-2 gap-10 pt-8 pb-8 mx-auto text-md">
             <p className="leading-6">{t.relevance1}</p>
             <p className="leading-6">{t.relevance2}</p>
           </div>
         </div>
       </div>
-      <div className="grid md:grid-cols-2 grid-cols-1 gap-10 container mx-auto mb-5">
-        <div className="md:cols-span-1 col-span-1 hidden md:block ">
+      <div className="container grid grid-cols-1 gap-10 mx-auto mb-5 md:grid-cols-2">
+        <div className="hidden col-span-1 md:cols-span-1 md:block ">
           <YMaps>
             <Map
               width="400px"
@@ -113,8 +113,8 @@ const About = () => {
         </div>
         <div>
           <div className="container mx-auto text-3xl font-bold">{t.rek}</div>
-          <div className="container mx-auto font-semibold text-md md:text-md py-5">
-            <table className="table-auto mb-10">
+          <div className="container py-5 mx-auto font-semibold text-md md:text-md">
+            <table className="mb-10 table-auto">
               <tbody>
                 <tr>
                   <td>Компания</td>
@@ -130,7 +130,7 @@ const About = () => {
                 </tr>
                 <tr>
                   <td>Филиал </td>
-                  <td>АО "Kaspi Bank" в г.Алматы</td>
+                  <td>АО Kaspi Bank в г.Алматы</td>
                 </tr>
                 <tr>
                   <td>ИИК</td>
